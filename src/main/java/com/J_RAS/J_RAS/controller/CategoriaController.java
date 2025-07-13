9d9d3349f8f75c84238ba546e86c9f6ebef6cb17
Stanley Serrano <so0152032019@unab.edu.sv>
2025-07-12 21:21:21 -0600

    feat(categoria): agrega CRUD en el controlador de categoria

--- src/main/java/com/J_RAS/J_RAS/controller/CategoriaController.java
+++ src/main/java/com/J_RAS/J_RAS/controller/CategoriaController.java
@@ -5,9 +5,7 @@ import com.J_RAS.J_RAS.model.CategoriaModel;
 import com.J_RAS.J_RAS.service.CategoriaService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,33 @@ public class CategoriaController {
         logger.info("Categorias obtenidas: ");
         categorias.forEach(categoria -> logger.info(categoria.toString()));
         return categorias;
+    }
+    @GetMapping
+    public CategoriaModel obtenerCategoriaPorId(Long idCategoria) {
+        CategoriaModel categoria = this.categoriaService.buscarCategoriaPorId(idCategoria);
+        if (categoria != null) {
+            logger.info("Categoria obtenida: " + categoria);
+            return categoria;
+        } else {
+            logger.warn("Categoria no encontrada con ID: " + idCategoria);
+            return null;
+        }
+    }
+    @PostMapping
+    public CategoriaModel agregarCategoria(CategoriaModel categoriaModel) {
+        logger.info("Categoria a agregar: " + categoriaModel);
+        CategoriaModel categoriaGuardada = this.categoriaService.guardarCategoria(categoriaModel);
+        logger.info("Categoria guardada: " + categoriaGuardada);
+        return categoriaGuardada;
+    }
+    @DeleteMapping(path = "/{id}")
+    public String eliminarCategoriaPorId(@PathVariable("id") Long id) {
+        boolean ok = this.categoriaService.eliminarCategoriaPorId(id);
+        if (ok) {
+            return "Se eliminó la categoría con ID " + id;
+        } else {
+            return "No se pudo eliminar la categoría con ID " + id;
+        }
     }
 
 }
